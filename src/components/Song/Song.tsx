828d0c788779ddddf070c99470a84b5d3aa597e9
santhosh <santhosh.mudavath@patent-art.com>
2024-06-07 10:51:30 +0530

    blobURL added for music

--- src/components/Song/Song.tsx
+++ src/components/Song/Song.tsx
@@ -1,54 +1,33 @@
-"use client";
-import React, { useState, useEffect, useRef } from 'react';
-import { FaPlay, FaPause, FaDownload } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
 
-const Song = ({ title, src }: any) => {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const audioRef: any = useRef(null);
+const AudioPlayer = ({ src, title }: any) => {
+    const [blobUrl, setBlobUrl] = useState('');
 
     useEffect(() => {
-        audioRef.current = new Audio(src);
-        return () => {
-            // Clean up the audio object when the component is unmounted
-            audioRef.current.pause();
-            audioRef.current = null;
-        };
+        fetch(src)
+            .then(response => response.blob())
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+                setBlobUrl(url);
+            });
     }, [src]);
-
-    const togglePlayPause = () => {
-        if (isPlaying) {
-            audioRef.current.pause();
-        } else {
-            audioRef.current.play();
-        }
-        setIsPlaying(!isPlaying);
-    };
 
     const handleDownload = () => {
         const link = document.createElement('a');
         const fileExtension = src.split('.').pop(); // Extract the file extension from the src
-        link.href = src;
+        link.href = blobUrl;
         link.download = `${title}.${fileExtension}`; // Use the extracted file extension
         link.click();
     };
 
-
     return (
-        <div className="flex items-center justify-between bg-gray-800 text-white p-4 rounded-lg mb-2">
-            <div>
-                <h2 className="text-lg font-bold">{title}</h2>
-            </div>
-            <div>
-                <audio ref={audioRef} src={src}></audio>
-                <button className="p-2 bg-blue-500 rounded-full" onClick={togglePlayPause}>
-                    {isPlaying ? <FaPause /> : <FaPlay />}
-                </button>
-                <button className="p-2 bg-gray-500 rounded-full ml-2" onClick={handleDownload}>
-                    <FaDownload />
-                </button>
-            </div>
+        <div>
+            <audio controls src={blobUrl}>
+                Your browser does not support the audio element.
+            </audio>
+            <button onClick={handleDownload}>Download</button>
         </div>
     );
 };
 
-export default Song;
+export default AudioPlayer;
